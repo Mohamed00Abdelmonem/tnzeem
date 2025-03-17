312928189575448d86abff4fb1eefdffaf4941a8
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-03-17 10:35:46 +0200

    fix endpoint

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -30,7 +30,7 @@ function OrderForm() {
           city: values.city,
           destination_country: values.destination_country,
         };
-        const response = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/crm/create-customer-wordpress/`, payload, {
+        const response = await axios.post(`https://api/tnzeem.net/crm/create-customer-wordpress/`, payload, {
           headers: {
             "Content-Type": "application/json",
           },
